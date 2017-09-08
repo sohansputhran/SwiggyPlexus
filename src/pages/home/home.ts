451c37146d467d47ba2c95d46835b1033e7b8162
Sohan S P <spsohan2106@gmail.com>
2017-09-08 20:03:22 +0530

    Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,15 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  items: any;
+  
   constructor(public navCtrl: NavController) {
-
+    this.items = [{name: "Empire", location: "Tamil Nadu", rating: "4.5",image:"assets/images/empire.jpg"},
+                  {name:"Green Leaf", location: "Karnataka", rating:"1",image:"assets/images/green_leaf.png"},
+                  {name:"AAAAA",location:"Kerala", rating: "2",image:"assets/images/empire.jpg"},
+                  
+                ]
+      
   }
 
 }
