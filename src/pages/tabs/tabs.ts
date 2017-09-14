7cfb6b3e84c74886eb9e8e4ff1b3f5172133f442
amrtiyashyam <shyam@plexus365.com>
2017-09-14 16:54:43 +0530

    images

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CartPage } from "../cart/cart";
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-
+import {AddToCartProvider} from "../../providers/add-to-cart/add-to-cart";
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -12,7 +12,11 @@ export class TabsPage {
   tab2Root = CartPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  constructor(public added :AddToCartProvider) {
 
+  
+  }
+  cartAdded(){
+    this.added.sendData();
   }
 }
